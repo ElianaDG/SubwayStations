8685770671f952a160c7c6be091c38a1318d3015
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-10-25 19:56:19 -0400

    finished connections list

--- src/main/java/subway/stations/SubwayStations.java
+++ src/main/java/subway/stations/SubwayStations.java
@@ -60,6 +60,90 @@ public class SubwayStations {
             int stationIdIndex = subwayLines.D.indexOf(stationId);
             connections.add(subwayLines.D.get(stationIdIndex + 1));
         }
+        if(subwayLines.E.contains(stationId)){
+            int stationIdIndex = subwayLines.E.indexOf(stationId);
+            connections.add(subwayLines.E.get(stationIdIndex + 1));
+        }
+        if(subwayLines.F.contains(stationId)){
+            int stationIdIndex = subwayLines.F.indexOf(stationId);
+            connections.add(subwayLines.F.get(stationIdIndex + 1));
+        }
+        if(subwayLines.G.contains(stationId)){
+            int stationIdIndex = subwayLines.G.indexOf(stationId);
+            connections.add(subwayLines.G.get(stationIdIndex + 1));
+        }
+        if(subwayLines.J.contains(stationId)){
+            int stationIdIndex = subwayLines.J.indexOf(stationId);
+            connections.add(subwayLines.J.get(stationIdIndex + 1));
+        }
+        if(subwayLines.L.contains(stationId)){
+            int stationIdIndex = subwayLines.L.indexOf(stationId);
+            connections.add(subwayLines.L.get(stationIdIndex + 1));
+        }
+        if(subwayLines.M.contains(stationId)){
+            int stationIdIndex = subwayLines.M.indexOf(stationId);
+            connections.add(subwayLines.M.get(stationIdIndex + 1));
+        }
+        if(subwayLines.N.contains(stationId)){
+            int stationIdIndex = subwayLines.N.indexOf(stationId);
+            connections.add(subwayLines.N.get(stationIdIndex + 1));
+        }
+        if(subwayLines.Q.contains(stationId)){
+            int stationIdIndex = subwayLines.Q.indexOf(stationId);
+            connections.add(subwayLines.Q.get(stationIdIndex + 1));
+        }
+        if(subwayLines.R.contains(stationId)){
+            int stationIdIndex = subwayLines.R.indexOf(stationId);
+            connections.add(subwayLines.R.get(stationIdIndex + 1));
+        }
+        if(subwayLines.S.contains(stationId)){
+            int stationIdIndex = subwayLines.S.indexOf(stationId);
+            connections.add(subwayLines.S.get(stationIdIndex + 1));
+        }
+        if(subwayLines.W.contains(stationId)){
+            int stationIdIndex = subwayLines.W.indexOf(stationId);
+            connections.add(subwayLines.W.get(stationIdIndex + 1));
+        }
+        if(subwayLines.Z.contains(stationId)){
+            int stationIdIndex = subwayLines.Z.indexOf(stationId);
+            connections.add(subwayLines.Z.get(stationIdIndex + 1));
+        }
+        if(subwayLines.sevenExpress.contains(stationId)){
+            int stationIdIndex = subwayLines.sevenExpress.indexOf(stationId);
+            connections.add(subwayLines.sevenExpress.get(stationIdIndex + 1));
+        }
+        if(subwayLines.sixExpress.contains(stationId)){
+            int stationIdIndex = subwayLines.sixExpress.indexOf(stationId);
+            connections.add(subwayLines.sixExpress.get(stationIdIndex + 1));
+        }
+        if(subwayLines.one.contains(stationId)){
+            int stationIdIndex = subwayLines.one.indexOf(stationId);
+            connections.add(subwayLines.one.get(stationIdIndex + 1));
+        }
+        if(subwayLines.two.contains(stationId)){
+            int stationIdIndex = subwayLines.two.indexOf(stationId);
+            connections.add(subwayLines.two.get(stationIdIndex + 1));
+        }
+        if(subwayLines.three.contains(stationId)){
+            int stationIdIndex = subwayLines.three.indexOf(stationId);
+            connections.add(subwayLines.three.get(stationIdIndex + 1));
+        }
+        if(subwayLines.four.contains(stationId)){
+            int stationIdIndex = subwayLines.four.indexOf(stationId);
+            connections.add(subwayLines.four.get(stationIdIndex + 1));
+        }
+        if(subwayLines.five.contains(stationId)){
+            int stationIdIndex = subwayLines.five.indexOf(stationId);
+            connections.add(subwayLines.five.get(stationIdIndex + 1));
+        }
+        if(subwayLines.six.contains(stationId)){
+            int stationIdIndex = subwayLines.six.indexOf(stationId);
+            connections.add(subwayLines.six.get(stationIdIndex + 1));
+        }
+        if(subwayLines.seven.contains(stationId)){
+            int stationIdIndex = subwayLines.seven.indexOf(stationId);
+            connections.add(subwayLines.seven.get(stationIdIndex + 1));
+        }
 
         return connections;
     }
