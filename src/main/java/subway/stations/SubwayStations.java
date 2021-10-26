4971fbf4d264eef2bf1a40515db3c507076c8ef1
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-10-26 16:57:13 -0400

    added condition check for arrayList

--- src/main/java/subway/stations/SubwayStations.java
+++ src/main/java/subway/stations/SubwayStations.java
@@ -21,7 +21,6 @@ public class SubwayStations {
         public int getObjectId(){return Integer.parseInt(properties.objectid);}
         public List<Double> getCoordinates(){return geometry.coordinates;}
     }
-<<<<<<< HEAD
 
     public List<String> getConnections(String stationName) throws IOException {
         Gson gson = new Gson();
@@ -47,109 +46,209 @@ public class SubwayStations {
 
         if(subwayLines.A.contains(stationId)){
             int stationIdIndex = subwayLines.A.indexOf(stationId);
-            connections.add(subwayLines.A.get(stationIdIndex + 1));
+            if(stationIdIndex < subwayLines.A.size() - 1){
+                connections.add(subwayLines.A.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.A.get(stationIdIndex - 1));
+            }
         }
         if(subwayLines.B.contains(stationId)){
             int stationIdIndex = subwayLines.B.indexOf(stationId);
-            connections.add(subwayLines.B.get(stationIdIndex + 1));
+            if(stationIdIndex < subwayLines.B.size() - 1){
+                connections.add(subwayLines.B.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.B.get(stationIdIndex - 1));
+            }
         }
         if(subwayLines.C.contains(stationId)){
             int stationIdIndex = subwayLines.C.indexOf(stationId);
-            connections.add(subwayLines.C.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.C.size() - 1){
+                connections.add(subwayLines.C.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.C.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.D.contains(stationId)){
             int stationIdIndex = subwayLines.D.indexOf(stationId);
-            connections.add(subwayLines.D.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.D.size() - 1){
+                connections.add(subwayLines.D.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.D.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.E.contains(stationId)){
             int stationIdIndex = subwayLines.E.indexOf(stationId);
-            connections.add(subwayLines.E.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.E.size() - 1){
+                connections.add(subwayLines.E.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.E.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.F.contains(stationId)){
             int stationIdIndex = subwayLines.F.indexOf(stationId);
-            connections.add(subwayLines.F.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.F.size() - 1){
+                connections.add(subwayLines.F.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.F.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.G.contains(stationId)){
             int stationIdIndex = subwayLines.G.indexOf(stationId);
-            connections.add(subwayLines.G.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.G.size() - 1){
+                connections.add(subwayLines.G.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.G.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.J.contains(stationId)){
             int stationIdIndex = subwayLines.J.indexOf(stationId);
-            connections.add(subwayLines.J.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.J.size() - 1){
+                connections.add(subwayLines.J.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.J.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.L.contains(stationId)){
             int stationIdIndex = subwayLines.L.indexOf(stationId);
-            connections.add(subwayLines.L.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.L.size() - 1){
+                connections.add(subwayLines.L.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.L.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.M.contains(stationId)){
             int stationIdIndex = subwayLines.M.indexOf(stationId);
-            connections.add(subwayLines.M.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.M.size() - 1){
+                connections.add(subwayLines.M.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.M.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.N.contains(stationId)){
             int stationIdIndex = subwayLines.N.indexOf(stationId);
-            connections.add(subwayLines.N.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.N.size() - 1){
+                connections.add(subwayLines.N.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.N.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.Q.contains(stationId)){
             int stationIdIndex = subwayLines.Q.indexOf(stationId);
-            connections.add(subwayLines.Q.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.Q.size() - 1){
+                connections.add(subwayLines.Q.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.Q.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.R.contains(stationId)){
             int stationIdIndex = subwayLines.R.indexOf(stationId);
-            connections.add(subwayLines.R.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.R.size() - 1){
+                connections.add(subwayLines.R.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.R.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.S.contains(stationId)){
             int stationIdIndex = subwayLines.S.indexOf(stationId);
-            connections.add(subwayLines.S.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.S.size() - 1){
+                connections.add(subwayLines.S.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.S.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.W.contains(stationId)){
             int stationIdIndex = subwayLines.W.indexOf(stationId);
-            connections.add(subwayLines.W.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.W.size() - 1){
+                connections.add(subwayLines.W.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.W.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.Z.contains(stationId)){
             int stationIdIndex = subwayLines.Z.indexOf(stationId);
-            connections.add(subwayLines.Z.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.Z.size() - 1){
+                connections.add(subwayLines.Z.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.Z.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.sevenExpress.contains(stationId)){
             int stationIdIndex = subwayLines.sevenExpress.indexOf(stationId);
-            connections.add(subwayLines.sevenExpress.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.sevenExpress.size() - 1){
+                connections.add(subwayLines.sevenExpress.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.sevenExpress.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.sixExpress.contains(stationId)){
             int stationIdIndex = subwayLines.sixExpress.indexOf(stationId);
-            connections.add(subwayLines.sixExpress.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.sixExpress.size() - 1){
+                connections.add(subwayLines.sixExpress.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.sixExpress.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.one.contains(stationId)){
             int stationIdIndex = subwayLines.one.indexOf(stationId);
-            connections.add(subwayLines.one.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.one.size() - 1){
+                    connections.add(subwayLines.one.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.one.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.two.contains(stationId)){
             int stationIdIndex = subwayLines.two.indexOf(stationId);
-            connections.add(subwayLines.two.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.two.size() - 1){
+                connections.add(subwayLines.two.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.two.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.three.contains(stationId)){
             int stationIdIndex = subwayLines.three.indexOf(stationId);
-            connections.add(subwayLines.three.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.three.size() - 1){
+                connections.add(subwayLines.three.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.three.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.four.contains(stationId)){
             int stationIdIndex = subwayLines.four.indexOf(stationId);
-            connections.add(subwayLines.four.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.four.size() - 1){
+                connections.add(subwayLines.four.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.four.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.five.contains(stationId)){
             int stationIdIndex = subwayLines.five.indexOf(stationId);
-            connections.add(subwayLines.five.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.five.size() - 1){
+                connections.add(subwayLines.five.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.five.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.six.contains(stationId)){
             int stationIdIndex = subwayLines.six.indexOf(stationId);
-            connections.add(subwayLines.six.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.six.size() - 1){
+                connections.add(subwayLines.six.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.six.get(stationIdIndex - 1));
+            }        }
         if(subwayLines.seven.contains(stationId)){
             int stationIdIndex = subwayLines.seven.indexOf(stationId);
-            connections.add(subwayLines.seven.get(stationIdIndex + 1));
-        }
+            if(stationIdIndex < subwayLines.seven.size() - 1){
+                connections.add(subwayLines.seven.get(stationIdIndex + 1));
+            }
+            if(stationIdIndex > 0){
+                connections.add(subwayLines.seven.get(stationIdIndex - 1));
+            }        }
 
         return connections;
     }
-=======
->>>>>>> 5f873781f2b9b5741fe005c08f44ec9af4fb36ac
 }
 
 class Properties{
